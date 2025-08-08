e957f9e9a767ec6754f255bbcef0e42e4ce61976
beachrockhotel <d.komarov2019@gmail.com>
2025-08-08 15:50:42 +0300

    inventory, payment services

--- order/cmd/main.go
+++ order/cmd/main.go
@@ -1,10 +1,97 @@
 package main
 
 import (
-	orderV1 "github.com/beachrockhotel/rocket-factory/pkg/openapi/order/v1"
+	"context"
+	"errors"
+	order_v1 "github.com/beachrockhotel/rocket-factory/shared/pkg/openapi/order/v1"
+	inventory_v1 "github.com/beachrockhotel/rocket-factory/shared/pkg/proto/inventory/v1"
+	payment_v1 "github.com/beachrockhotel/rocket-factory/shared/pkg/proto/payment/v1"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
+const (
+	httpPort = "8080"
+	// Таймауты для HTTP-сервера
+	readHeaderTimeout = 5 * time.Second
+	shutdownTimeout   = 10 * time.Second
+)
+
+type OrderStorage struct {
+	mu     sync.RWMutex
+	orders map[string]*order_v1.OrderDto
+}
+
+func NewOrderStorage() *OrderStorage {
+	return &OrderStorage{
+		orders: make(map[string]*order_v1.OrderDto),
+	}
+}
+
+type OrderHandler struct {
+	storage *OrderStorage
+}
+
+func NewOrderHandler(storage *OrderStorage) *OrderHandler {
+	return &OrderHandler{
+		storage: storage,
+	}
+}
+
 func main() {
-	storage := models.New
-	chi := new.ChiRouter
+	storage := NewOrderStorage()
+	orderHandler := NewOrderHandler(storage)
+	orderServer, err := order_v1.NewServer(orderHandler)
+	if err != nil {
+		log.Fatalf("ошибка создания сервера OpenAPI: %v", err)
+	}
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(10 * time.Second))
+
+	r.Mount("/", orderServer)
+
+	server := &http.Server{
+		Addr:        net.JoinHostPort("localhost", httpPort),
+		Handler:     r,
+		ReadTimeout: readHeaderTimeout, // Защита от Slowloris атак - тип DDoS-атаки, при которой
+		// атакующий умышленно медленно отправляет HTTP-заголовки, удерживая соединения открытыми и истощая
+		// пул доступных соединений на сервере. ReadHeaderTimeout принудительно закрывает соединение,
+		// если клиент не успел отправить все заголовки за отведенное время.
+	}
+
+	go func() {
+		log.Printf("🚀 HTTP-сервер запущен на порту %s\n", httpPort)
+		err = server.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("❌ Ошибка запуска сервера: %v\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("🛑 Завершение работы")
+
+	ctx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	err = server.Shutdown(ctx)
+	if err != nil {
+		log.Printf("Ошибка при остановке сервера: %v\n", err)
+	}
+	log.Println("✅ Сервер остановлен")
 }
